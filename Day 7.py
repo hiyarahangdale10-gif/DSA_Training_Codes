3a6790b908f4b317b1088262a00bac956776d7ad
https://github.com/hiyarahangdale10-gif/emotion-detector.git <https://github.com/hiyarahangdale10-gif/emotion-detector.git>
2026-05-19 16:25:00 +0530

    Day 7

--- Day 7.py
+++ Day 7.py
@@ -154,7 +154,7 @@
 
 
 
-# Implement astack using singly linked list
+# Implement a stack using singly linked list
 # import sys
 # class GetNode:
 #     def __init__(self):
@@ -241,3 +241,465 @@
 #         ans.append(2)
 # for i in range(N):
 #     print(ans[i] ** 3, end=" ")
+
+
+
+
+# Implement queue using Linked list
+# class GetNode:
+#     def __init__(self):
+#         self.data = None
+#         self.next = None
+# class Queue:
+#     def __init__(self):
+#         self.head = None
+#         self.rear = None
+#     def append(self):
+#         data = int(input("Enter the data : "))
+#         newNode = GetNode()
+#         newNode.data = data
+#         if self.head is None:
+#             self.head = newNode
+#             self.rear = newNode
+#         else:
+#             self.rear.next = newNode
+#             self.rear = newNode
+#         print(data, "inserted into queue")
+#     def deleteAtBegin(self):
+#         if self.head is None:
+#             print("Queue is empty")
+#         else:
+#             ptr = self.head
+#             self.head = ptr.next
+#             if self.head is None:
+#                 self.rear = None
+#             print(ptr.data, "is deleted")
+#     def traverse(self):
+#         if self.head is None:
+#             print("Queue is empty")
+#         else:
+#             ptr = self.head
+#             while ptr is not None:
+#                 print(ptr.data, end=" ")
+#                 ptr = ptr.next
+#             print()
+# q = Queue()
+# while True:
+#     print("\n1.Insert")
+#     print("2.Delete")
+#     print("3.Traverse")
+#     print("0.Exit")
+#     choice = int(input("Enter choice : "))
+#     if choice == 1:
+#         q.append()
+#     elif choice == 2:
+#         q.deleteAtBegin()
+#     elif choice == 3:
+#         q.traverse()
+#     elif choice == 0:
+#         break
+#     else:
+#         print("Invalid Choice")
+
+
+
+# import sys
+# class GetNode:
+# 	def __init__(self,data):
+# 		self.data=data
+# 		self.link=None
+# class LinkedList:
+# 	def __init__(self):
+# 		self.head=None
+# 		#ptr=GetNode()
+# 	def append(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		#new_node.data=data
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				ptr=ptr.link
+# 			ptr.link=new_node
+# 		print(data,"is added..")
+# 	def traverse(self):
+# 		ptr=self.head
+# 		while ptr is not None:
+# 			print(" -> ",ptr.data,end="")
+# 			ptr=ptr.link
+# 	def addAtBegin(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		#new_node.data=data
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			new_node.link=self.head
+# 			self.head=new_node
+# 		print(data,"is added at begin..")
+# 	def addAtAfter(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		key=int(input("Enter data after which data will be added: "))
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr=ptr.link
+# 			if ptr.link is None:
+# 				print(key," not found ")
+# 			else:
+# 				ptr1=ptr.link
+# 				new_node.link=ptr1
+# 				ptr.link=new_node
+# 				print(data,"is added after ",key)
+# 	def addAtBefore(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		key=int(input("Enter data before which data will be added: "))
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr1=ptr
+# 					ptr=ptr.link
+# 			if ptr.link is None:
+# 				print(key," not found ")
+# 			else:
+# 				new_node.link=ptr
+# 				ptr1.link=new_node
+# 				print(data,"is added after ",key)
+# 	def addAtEnd(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				ptr=ptr.link
+# 			ptr.link=new_node
+# 		print(data,"is added at the end..")
+# 	def deleteAtBegin(self):
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			ptr1=ptr.link
+# 			ptr.link=None
+# 			self.head=ptr1
+# 		print(ptr.data,"is deleted from begin..")
+# 	def deleteAtAfter(self):
+# 		key=int(input("Enter data after which data will be added: "))
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr=ptr.link
+# 			if ptr.link is None:
+# 				print(key," not found ")
+# 			else:
+# 				ptr1=ptr.link
+# 				ptr2=ptr1.link
+# 				ptr.link=ptr2
+# 				ptr1.link=None
+# 				print(ptr1.data," is deleted after ",key)
+# 	def deleteAtValue(self):
+# 		key=int(input("Enter data which node will be deleted: "))
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr1=ptr
+# 					ptr=ptr.link
+# 					ptr2=ptr.link
+# 			if ptr.link is None:
+# 				print(key," not found ")
+# 			else:				
+# 				ptr1.link=ptr2
+# 				ptr.link=None
+# 				print(ptr.data,"is deleted ",key)
+# 	def deleteAtEnd(self):
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.link is not None:
+# 				ptr1=ptr
+# 				ptr=ptr.link
+# 			ptr1.link=None
+# 			print(ptr.data,"is deleted from the end..")	
+# 	def length(self):
+# 		len=0
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr is not None:
+# 				len=len+1
+# 				ptr=ptr.link
+# 			print("Size of LinkedList is ",len)
+
+# obj=LinkedList()
+# if __name__ == '__main__':
+# 	while True:
+# 		print("\n\n1. Append\t\t2. Add At Begin  \t3. Add At After")
+# 		print("4. Add At Before\t5. Add At End\t\t6. Delete At Begin")
+# 		print("7. Delete At After\t8. Delete Ay Node\t9. Delete At End")
+# 		print("10. Length\t\t11. Traverse\t\t0. Exit")
+# 		n=int(input("Select your choice: "))
+# 		if n==1:
+# 			obj.append()
+# 		elif n==2:
+# 			obj.addAtBegin()
+# 		elif n==3:
+# 			obj.addAtAfter()
+# 		elif n==4:
+# 			obj.addAtBefore()
+# 		elif n==5:
+# 			obj.addAtEnd()
+# 		elif n==6:
+# 			obj.deleteAtBegin()
+# 		elif n==7:
+# 			obj.deleteAtAfter()
+# 		elif n==8:
+# 			obj.deleteAtValue()
+# 		elif n==9:
+# 			obj.deleteAtEnd()
+# 		elif n==10:
+# 			obj.length()
+# 		elif n==11:
+# 			obj.traverse()
+# 		elif n==0:
+# 			sys.exit()
+
+
+
+# import sys
+# class GetNode:
+# 	def __init__(self,data):
+# 		self.data=data
+# 		self.left=None
+# 		self.right=None
+# class LinkedList:
+# 	def __init__(self):
+# 		self.head=None
+# 		#ptr=GetNode()
+# 	def append(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		#new_node.data=data
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				ptr=ptr.right
+# 			ptr.right=new_node
+# 			new_node.left=ptr
+# 			print(data,"is added..")
+# 	def traverseForward(self):
+# 		ptr=self.head
+# 		while ptr is not None:
+# 			print(" ->",ptr.data,end="")
+# 			ptr=ptr.right
+# 	def traverseBackward(self):
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				ptr=ptr.right
+# 			while ptr is not None:
+# 				print(" ->",ptr.data,end="")
+# 				ptr=ptr.left
+# 	def addAtBegin(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		#new_node.data=data
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			new_node.right=self.head
+# 			self.head.left=new_node
+# 			self.head=new_node
+# 			print(data,"is added at begin..")
+# 	def addAtAfter(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		key=int(input("Enter data after which data will be added: "))
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr=ptr.right
+# 			if ptr.right is None:
+# 				print(key," not found ")
+# 			else:
+# 				ptr1=ptr.right
+# 				new_node.right=ptr1
+# 				ptr1.left=new_node
+# 				ptr.right=new_node
+# 				new_node.left=ptr
+# 				print(data,"is added after ",key)
+# 	def addAtBefore(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		key=int(input("Enter data after which data will be added: "))
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr=ptr.right
+# 			if ptr.right is None:
+# 				print(key," not found ")
+# 			else:
+# 				ptr1=ptr.left
+# 				new_node.right=ptr
+# 				ptr.left=new_node
+# 				ptr1.right=new_node
+# 				new_node.left=ptr1
+# 				print(data,"is added before ",key)
+# 	def addAtEnd(self):
+# 		data=int(input("Enter data: "))
+# 		new_node=GetNode(data)
+# 		#new_node.data=data
+# 		if self.head is None:
+# 			self.head=new_node
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				ptr=ptr.right
+# 			ptr.right=new_node
+# 			new_node.left=ptr
+# 			print(data,"is added at the end..")
+# 	def deleteAtBegin(self):
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			ptr1=ptr.right
+# 			ptr.right=None
+# 			self.head=ptr1
+# 			print(ptr.data,"is deleted from begin..")
+# 	def deleteAtAfter(self):
+# 		key=int(input("Enter data after which data will be added: "))
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr=ptr.right
+# 			if ptr.right is None:
+# 				print(key," not found ")
+# 			else:
+# 				ptr1=ptr.right
+# 				ptr2=ptr1.right
+# 				ptr.right=ptr2
+# 				ptr2.left=ptr
+# 				ptr1.right=None
+# 				print(ptr1.data," is deleted after ",key)
+# 	def deleteAtValue(self):
+# 		key=int(input("Enter data which node will be deleted: "))
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				if key==ptr.data:
+# 					break
+# 				else:
+# 					ptr=ptr.right
+# 			if ptr.right is None:
+# 				print(key," not found ")
+# 			else:
+# 				ptr1=ptr.left
+# 				ptr2=ptr.right
+# 				ptr1.right=ptr2
+# 				ptr2.left=ptr1
+# 				ptr.right=None
+# 				print(ptr.data,"is deleted at ",key)
+# 	def deleteAtEnd(self):
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr.right is not None:
+# 				ptr=ptr.right
+# 			ptr1=ptr.left
+# 			ptr1.right=None
+# 			print(ptr.data,"is deleted from the end..")	
+# 	def length(self):
+# 		len=0
+# 		if self.head is None:
+# 			print("LinkedList is empty..")
+# 		else:
+# 			ptr=self.head
+# 			while ptr is not None:
+# 				len=len+1
+# 				ptr=ptr.right
+# 			print("Size of LinkedList is ",len)
+
+# obj=LinkedList()
+# if __name__ == '__main__':
+# 	while True:
+# 		print("\n\n1. Append\t\t2. Add At Begin  \t3. Add At After")
+# 		print("4. Add At Before\t5. Add At End\t\t6. Delete At Begin")
+# 		print("7. Delete At After\t8. Delete Ay Node\t9. Delete At End")
+# 		print("10. Length\t\t11. TraverseForward\t12. TraverseBackward \n0. Exit")
+# 		n=int(input("Select your choice: "))
+# 		if n==1:
+# 			obj.append()
+# 		elif n==2:
+# 			obj.addAtBegin()
+# 		elif n==3:
+# 			obj.addAtAfter()
+# 		elif n==4:
+# 			obj.addAtBefore()
+# 		elif n==5:
+# 			obj.addAtEnd()
+# 		elif n==6:
+# 			obj.deleteAtBegin()
+# 		elif n==7:
+# 			obj.deleteAtAfter()
+# 		elif n==8:
+# 			obj.deleteAtValue()
+# 		elif n==9:
+# 			obj.deleteAtEnd()
+# 		elif n==10:
+# 			obj.length()
+# 		elif n==11:
+# 			obj.traverseForward()
+# 		elif n==12:
+# 			obj.traverseBackward()
+# 		elif n==0:
+# 			sys.exit()
